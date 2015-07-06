f4fb0913870a39b79830a1dee7173b23cd976a9d
datLucius <jlnelsonart@gmail.com>
2015-07-06 14:29:04 -0400

    new strat for navigation, about to make an overhaul and probably destroy everything, cheers

---
+++ nav.js
@@ -0,0 +1,23 @@
+(function(){
+    var app = angular.module('nav-directives', []);
+
+    app.directive("navigateTabs", function() {
+      return {
+        restrict: "E",
+        templateUrl: "navigate-tabs.html",
+        controller: function() {
+          this.tab = 1;
+
+          this.isSet = function(checkTab) {
+            return this.tab === checkTab;
+          };
+
+          this.setTab = function(activeTab) {
+            this.tab = activeTab;
+          };
+        },
+        controllerAs: "tab"
+      };
+    });
+
+  })();
